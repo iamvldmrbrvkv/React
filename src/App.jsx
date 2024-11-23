1f0b8986373f03c421d440203f05f825b08dcf77
Владимир Боровиков <iamvladimirborovikov@gmail.com>
2024-11-24 01:42:57 +0300

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,110 @@
+/* Задание:
+
+1. Починить чекбоксы
+
+2. Починить отображение количества рендеров в круглых скобках в названии чекбокса
+
+3. Исправить ошибки в коде
+
+Задание со звездочкой: Сделать так чтобы ререндер происходил только у изменяемого элемента или предложить в комментарии в коде или в ответном письме варианты как этого можно достичь.
+
+Комментарий: На вкладке Problems должно быть пусто и в решении не должно быть неиспользованных функций. */
+
+//Функции getList и setList - не требуют изменений
+
+/* import React, { useState, useEffect, useRef } from "react";
+
+function getList() {
+  //Код функции не требует изменений
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve(
+        Array.from({ length: 10 }, (_el, index) => ({
+          label: `label ${index + 1}`,
+          checked: false,
+          id: index
+        }))
+      );
+    }, 1000);
+  });
+}
+
+function setList(list) {
+  //Код функции не требует изменений
+}
+
+const Row = (props) => {
+  const renders = useRef(0);
+
+  const handleChange = (id) => {
+    props.handleChange(id);
+  };
+
+  const { label, checked, id } = props;
+
+  return (
+    <div>
+      {label}
+
+      {` (${renders.current++}) `}
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={() => handleChange(id)}
+      />
+    </div>
+  );
+};
+
+const List = ({ list }) => {
+  if (list.length === 0) {
+    return <div>LOADING...</div>;
+  }
+
+  const [localList, setLocalList] = useState(null);
+
+  const handleChange = (id) => {
+    localList[id].checked = !localList[id].checked;
+  };
+
+  useEffect(() => {
+    setList(localList);
+  });
+
+  useEffect(() => {
+    setLocalList(list);
+  });
+
+  return (
+    <>
+      {localList?.map((item) => (
+        <Row {...item} handleChange={handleChange} />
+      ))}
+    </>
+  );
+};
+
+const Test = () => {
+  const [listRow, setListRow] = useState([]);
+
+  useEffect(() => {
+    getList().then((res) => {
+      setListRow(res);
+    });
+  }, []);
+
+  return (
+    <div class="test">
+      <h1>Test</h1>
+      <List list={listRow} />
+    </div>
+  );
+};
+
+Test.defaultProps = {};
+
+export default Test; */
+
 // первое оригинальное решение
 
 /* //Функции getList и setList - не требуют изменений
@@ -93,10 +200,10 @@ export default Test;
 
 // задание со звездочкой
 
-// Решение
-// 1. Обернуть handleChange в useCallback чтобы кешировать фукнцию, так как при изменении стейта localList происходил ререндер компонента List и фунция handleChange создавалась заново
-// 2. Обернуть в memo компонент Row чтобы перерисовке родительского компонента List ререндерились только те компоненты Row которые получили новые пропсы, в нашем случае новым пропсом может быть только checked
-// 3. Так как мы передаем handleChange как пропс в Row, этот пропс вызывал бы перерисовку всех Row, но мы ее кешировали с помошью useCallback это будет одна и та же неизменная handleChange которая не приведет к ререндеру Row
+/* Решение
+1. Обернуть handleChange в useCallback чтобы кешировать фукнцию, так как при изменении стейта localList происходил ререндер компонента List и фунция handleChange создавалась заново
+2. Обернуть в memo компонент Row чтобы перерисовке родительского компонента List ререндерились только те компоненты Row которые получили новые пропсы, в нашем случае новым пропсом может быть только checked
+3. Так как мы передаем handleChange как пропс в Row, этот пропс вызывал бы перерисовку всех Row, но мы ее кешировали с помошью useCallback это будет одна и та же неизменная handleChange которая не приведет к ререндеру Row */
 
 
 //Функции getList и setList - не требуют изменений
