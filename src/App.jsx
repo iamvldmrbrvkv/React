a5ea9a1d1286cf42f9bb588f4cc917137f54239b
Владимир Боровиков <iamvladimirborovikov@gmail.com>
2024-11-23 22:06:31 +0300

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-//Функции getList и setList - не требуют изменений
+// первое оригинальное решение
+
+/* //Функции getList и setList - не требуют изменений
 
 import React, { useState, useEffect, useRef } from "react";
 
@@ -44,7 +46,6 @@ const List = ({ list }) => {
     return <div>LOADING...</div>;
   }
 
-  // лист в локальный лист теперь у листа свое состояние если оно изменится компонент перерисуется
   const [localList, setLocalList] = useState(null);
 
   const handleChange = (id) => {
@@ -59,7 +60,7 @@ const List = ({ list }) => {
 
   useEffect(() => {
     setLocalList(list);
-  }, [list]);
+  }, []);
 
   return (
     <>
@@ -89,6 +90,102 @@ const Test = () => {
   );
 };
 
-// Test.defaultProps = {};
+export default Test;
+ */
+
+// задание со звездочкой
+
+// Решение
+// 1. Обернуть handleChange в useCallback чтобы кешировать фукнцию, так как при изменении стейта localList происходил ререндер компонента List и фунция handleChange создавалась заново
+// 2. Обернуть в memo компонент Row чтобы перерисовке родительского компонента List ререндерились только те компоненты Row которые получили новые пропсы, в нашем случае новым пропсом может быть только checked
+// 3. Так как мы передаем handleChange как пропс в Row, этот пропс вызывал бы перерисовку всех Row, но мы ее кешировали с помошью useCallback это будет одна и та же неизменная handleChange которая не приведет к ререндеру Row
+
+
+//Функции getList и setList - не требуют изменений
+
+import React, { useState, useEffect, useRef, useCallback, memo } from "react";
+
+function getList() {
+  //Код функции не требует изменений
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve(
+        Array.from({ length: 10 }, (_el, index) => ({
+          label: `label ${index + 1}`,
+          checked: false,
+          id: index
+        }))
+      );
+    }, 1000);
+  });
+}
+
+function setList(list) {
+  //Код функции не требует изменений
+}
+
+const Row = memo(function Row({ label, checked, id, handleChange }) {
+  const renders = useRef(0)
+  return (
+    <div>
+      {label}
+
+      {` (${renders.current++}) `}
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={() => handleChange(id)}
+      />
+    </div>
+  );
+});
+
+const List = ({ list }) => {
+  if (list.length === 0) {
+    return <div>LOADING...</div>;
+  }
+  const [localList, setLocalList] = useState(null);
+
+  const handleChange = useCallback((id) => {
+    setLocalList((prev) =>
+      prev.map((item) =>
+        item.id === id
+          ? { ...item, checked: !item.checked }
+          : item
+      )
+    );    
+  }, []);
+
+  useEffect(() => {
+    setLocalList(list);
+  }, []);
+
+  return (
+    <>
+      {localList?.map(item => {
+        const { label, checked, id } = item
+        return <Row key={id} label={label} checked={checked} id={id} handleChange={handleChange} />
+      })}
+    </>
+  );
+};
+
+const Test = () => {
+  const [listRow, setListRow] = useState([]);
+
+  useEffect(() => {
+    getList().then((res) => {
+      setListRow(res);
+    });
+  }, []);
+
+  return (
+    <div className="test">
+      <h1>Test</h1>
+      <List list={listRow} />
+      {console.log('listRow', listRow)}
+    </div>
+  );
+};
 
 export default Test;
